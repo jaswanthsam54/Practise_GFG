744a66e9520bdcbcf889d1912b11c50c3bf71e10
Jaswanth Saminepally <saminepallyjaswanth@gmail.com>
2026-06-04 12:38:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Upper Bound/implement-upper-bound.java
@@ -0,0 +1,17 @@
+class Solution {
+    int upperBound(int[] arr, int target) {
+        // code here
+        int lo = 0;
+        int hi = arr.length;
+        while (lo < hi) {
+            int mid = lo + (hi - lo)/2;
+            if (arr[mid] > target){
+                hi = mid;
+            }
+            else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+}

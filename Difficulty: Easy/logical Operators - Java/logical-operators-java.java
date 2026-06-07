0bb163beda0baecb196ecf4aab0f56ef3cca0356
Jaswanth Saminepally <saminepallyjaswanth@gmail.com>
2026-06-07 09:22:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/logical Operators - Java/logical-operators-java.java
@@ -0,0 +1,10 @@
+class Geeks {
+
+    static void logicOp(boolean a, boolean b) {
+        /*output (a&&b), (a||b), and ((!a)&&(!b))separated by spac
+        e0s*/
+        System.out.print((a && b) + " " + (a || b) + " " + ((!a) && (!b)));
+    
+    }
+    
+}

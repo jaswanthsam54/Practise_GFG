3a73a0f4a0018ab523d899a2d5c7c7c9a3a39176
Jaswanth Saminepally <saminepallyjaswanth@gmail.com>
2026-06-13 23:07:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Static Method In Python/static-method-in-python.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+# Implement Addition class here
+# User function Template for python3
+
+class Addition:
+    @staticmethod
+    def add(a: int, b: int) -> int:
+        return a + b

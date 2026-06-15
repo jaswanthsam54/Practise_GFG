f3c122772f3053170d2f6a762b8595ac74eb9f26
Jaswanth Saminepally <saminepallyjaswanth@gmail.com>
2026-06-15 23:09:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Prime Number/next-prime-number.java
+++ Difficulty: Medium/Next Prime Number/next-prime-number.java
@@ -1,9 +1,8 @@
-// User function Template for Java
 class Solution {
     public static int nextPrime(int n) {
         int candidate = n + 1;
         
-        // Loop infinitely until we find the next prime
+        // Keep incrementing candidate until we find a prime number
         while (true) {
             if (isPrime(candidate)) {
                 return candidate;
@@ -18,7 +17,7 @@ class Solution {
         if (num == 2 || num == 3) return true;
         if (num % 2 == 0 || num % 3 == 0) return false;
         
-        // Check potential factors up to the square root of num
+        // Check factors up to the square root of num
         for (int i = 5; i * i <= num; i += 6) {
             if (num % i == 0 || num % (i + 2) == 0) {
                 return false;

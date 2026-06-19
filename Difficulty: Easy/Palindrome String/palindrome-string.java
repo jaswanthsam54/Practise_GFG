9ff0109a30898e3e884ac9d436a5e39fc89f28f1
Jaswanth Saminepally <saminepallyjaswanth@gmail.com>
2026-06-19 23:02:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome String/palindrome-string.java
+++ Difficulty: Easy/Palindrome String/palindrome-string.java
@@ -1,8 +1,19 @@
 class Solution {
     boolean isPalindrome(String s) {
         // code here
-        StringBuilder sb = new StringBuilder(s);
-        String rev = sb.reverse().toString();
-        return s.equals(rev);
+        int left = 0;
+        int right = s.length() - 1;
+        
+        // Move pointers towards the center
+        while (left < right) {
+            // If characters at front and back don't match, it's not a palindrome
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
     }
 }
